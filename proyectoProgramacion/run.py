b7a779b795bdf4089eafa823b254fd4c21564c68
reroes <reroes799@gmail.com>
2019-04-23 22:43:08 -0500

    nuevos cambios en la rama5

--- proyectoProgramacion/run.py
+++ proyectoProgramacion/run.py
@@ -5,5 +5,8 @@ from mimodulo import *
 
 for i in range(0, rango):
     print("%d" %((i + opcion)))
+    print("%d" %((i + opcion)))
     print("----")
+    print("----")
+    print("%d : " % (i))
     print("%d : " % (i))
